5aabcb76bb1fcc101e40ff5862d70779384a1c76
Smokey <kmboplays@gmail.com>
2020-06-16 10:07:30 +0500

    added fusion.sh script

---
+++ fusion.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# This Script needs one change from the users and have some instructions how to use it, so please do read first 10 Lines of the Script.
+
+# Clone this script in your ROM Repo using following commands.
+# cd rom_repo
+# curl https://raw.githubusercontent.com/smokey18/LegacyServer/master/fusion.sh > script_build.sh
+
+# Some User's Details. Please fill it with your own details.
+
+# Replace "legacy" with your own SSH Username in lowercase
+username=hassan
+
+# Colors makes things beautiful
+export TERM=xterm
+
+    red=$(tput setaf 1)             #  red
+    grn=$(tput setaf 2)             #  green
+    blu=$(tput setaf 4)             #  blue
+    cya=$(tput setaf 6)             #  cyan
+    txtrst=$(tput sgr0)             #  Reset
+
+# CCACHE UMMM!!! Cooks my builds fast
+
+if [ "$use_ccache" = "yes" ];
+then
+echo -e ${blu}"CCACHE is enabled for this build"${txtrst}
+export CCACHE_EXEC=$(which ccache)
+export USE_CCACHE=1
+export CCACHE_DIR=/home/ccache/$username
+ccache -M 50G
+fi
+
+if [ "$use_ccache" = "clean" ];
+then
+export CCACHE_EXEC=$(which ccache)
+export CCACHE_DIR=/home/ccache/$username
+ccache -C
+export USE_CCACHE=1
+ccache -M 50G
+wait
+echo -e ${grn}"CCACHE Cleared"${txtrst};
+fi
+
+# Its Clean Time
+if [ "$make_clean" = "yes" ];
+then
+make clean && make clobber
+wait
+echo -e ${cya}"OUT dir from your repo deleted"${txtrst};
+fi
+
+# Its Images Clean Time
+if [ "$make_clean" = "installclean" ];
+then
+make installclean
+wait
+echo -e ${cya}"Images deleted from OUT dir"${txtrst};
+fi
+
+# Build ROM
+source build/envsetup.sh
+lunch aosp_mido-userdebug
+mka bacon -j"$jobs"
